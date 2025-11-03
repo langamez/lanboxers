51ec60244282e2cb36ac7706ddad023619fc469c
a.tabatabaei <a.tabatabaei@sanbod.co>
2025-11-03 20:28:54 +0330

    Update: Add: hit action& animation & playerTwo

--- main.go
+++ main.go
@@ -38,62 +38,189 @@ func spawnCage(cage Cage) {
 	fmt.Printf("\033[%d;1H", cage.Limit.Y+10)
 }
 
-func spawnBoxer(cage Cage, boxer Boxer, Hit string) {
+func spawnBoxers(cage Cage, boxerOne Boxer, boxerTwo Boxer, Hit string) {
 	//Initialize chars of body
 	var (
-		head  = ":)"
-		reset = "\033[0m"
+		boxerOneHead = ":)"
+		boxerTwoHead = "(:"
+		reset        = "\033[0m"
 
-		leftArm, rightArm           string
-		leftShoulder, rightShoulder string
+		// player 1
+		boxerOneLeftArm, boxerOneRightArm           string
+		boxerOneLeftShoulder, boxerOneRightShoulder string
+		// player 2
+		boxerTwoLeftArm, boxerTwoRightArm           string
+		boxerTwoLeftShoulder, boxerTwoRightShoulder string
 	)
 
 	// Initialize body part poses
-	leftArmPose := Position{X: boxer.Pose.X + 1, Y: boxer.Pose.Y - 2}
-	rightArmPose := Position{X: boxer.Pose.X + 1, Y: boxer.Pose.Y + 2}
+	// player 1 body parts position
+	boxerOneLeftArmPose := Position{X: boxerOne.Pose.X + 1, Y: boxerOne.Pose.Y - 2}
+	boxerOneRightArmPose := Position{X: boxerOne.Pose.X + 1, Y: boxerOne.Pose.Y + 2}
 
-	leftShoulderPose := Position{X: leftArmPose.X, Y: boxer.Pose.Y - 1}
-	rightShoulderPose := Position{X: rightArmPose.X, Y: boxer.Pose.Y + 1}
+	boxerOneLeftShoulderPose := Position{X: boxerOneLeftArmPose.X, Y: boxerOne.Pose.Y - 1}
+	boxerOneRightShoulderPose := Position{X: boxerOneRightArmPose.X, Y: boxerOne.Pose.Y + 1}
+
+	// player 2 body parts position
+	boxerTwoLeftArmPose := Position{X: boxerTwo.Pose.X - 4, Y: boxerTwo.Pose.Y + 2}
+	boxerTwoRightArmPose := Position{X: boxerTwo.Pose.X - 4, Y: boxerTwo.Pose.Y - 2}
+
+	boxerTwoLeftShoulderPose := Position{X: boxerTwoLeftArmPose.X + 3, Y: boxerTwo.Pose.Y + 1}
+	boxerTwoRightShoulderPose := Position{X: boxerTwoRightArmPose.X + 3, Y: boxerTwo.Pose.Y - 1}
 
 	if Hit != "" {
 		switch Hit {
-		case "UP":
-			leftShoulder = "()═══O"
-			leftShoulderPose = Position{X: leftShoulderPose.X + 1, Y: boxer.Pose.Y}
-		case "DOWN":
-			rightShoulder = "()═══O"
-			rightShoulderPose = Position{X: rightShoulderPose.X + 1, Y: boxer.Pose.Y}
+		case "upInit":
+			// player 1
+			boxerOneLeftArm = ""
+			boxerOneRightArm = "╰╰══O"
+			boxerOneRightShoulder = "()"
+			boxerOneLeftShoulder = "()══O"
+			boxerOneLeftShoulderPose = Position{X: boxerOneLeftShoulderPose.X + 1, Y: boxerOneLeftShoulderPose.Y}
+
+			// player 2
+			boxerTwoLeftArm = "O══╯╯"
+			boxerTwoRightArm = "O══╮╮"
+			boxerTwoLeftShoulder = "()"
+			boxerTwoRightShoulder = "()"
+		case "up":
+			// player 1
+			boxerOneLeftArm = ""
+			boxerOneRightArm = "╰╰══O"
+			boxerOneRightShoulder = "()"
+			boxerOneLeftShoulder = "()═══O"
+			boxerOneLeftShoulderPose = Position{X: boxerOneLeftShoulderPose.X + 1, Y: boxerOneLeftShoulderPose.Y}
+
+			// player 2
+			boxerTwoHead = "),: **"
+			boxerTwoLeftArm = "O══╯╯"
+			boxerTwoRightArm = "O══╮╮"
+			boxerTwoLeftShoulder = "()"
+			boxerTwoRightShoulder = "()"
+		case "upHit":
+			// player 1
+			boxerOneLeftArm = ""
+			boxerOneRightArm = "╰╰══O"
+			boxerOneRightShoulder = "()"
+			boxerOneLeftShoulder = "()═══O"
+			boxerOneLeftShoulderPose = Position{X: boxerOneLeftShoulderPose.X + 1, Y: boxerOneLeftShoulderPose.Y}
+
+			// player 2
+			boxerTwoHead = "),: **"
+			boxerTwoLeftArm = "O══╯╯"
+			boxerTwoRightArm = "O══╮╮"
+			boxerTwoLeftShoulder = "()"
+			boxerTwoRightShoulder = "()"
+		case "downInit":
+			// player 1
+			boxerOneRightArm = ""
+			boxerOneLeftArm = "╭╭══O"
+			boxerOneLeftShoulder = "()"
+			boxerOneRightShoulder = "()══O"
+			boxerOneRightShoulderPose = Position{X: boxerOneRightShoulderPose.X + 1, Y: boxerOneRightShoulderPose.Y}
+
+			// player 2
+			boxerTwoLeftArm = "O══╯╯"
+			boxerTwoRightArm = "O══╮╮"
+			boxerTwoLeftShoulder = "()"
+			boxerTwoRightShoulder = "()"
+		case "down":
+			// player 1
+			boxerOneRightArm = ""
+			boxerOneLeftArm = "╭╭══O"
+			boxerOneLeftShoulder = "()"
+			boxerOneRightShoulder = "()═══O"
+			boxerOneRightShoulderPose = Position{X: boxerOneRightShoulderPose.X + 1, Y: boxerOneRightShoulderPose.Y}
+
+			// player 2
+			boxerTwoHead = "),: **"
+			boxerTwoLeftArm = "O══╯╯"
+			boxerTwoRightArm = "O══╮╮"
+			boxerTwoLeftShoulder = "()"
+			boxerTwoRightShoulder = "()"
+		case "downHit":
+			switch "actor" {
+			case "one":
+				// player 1
+				boxerOneRightArm = ""
+				boxerOneLeftArm = "╭╭══O"
+				boxerOneLeftShoulder = "()"
+				boxerOneRightShoulder = "()═══O"
+				boxerOneRightShoulderPose = Position{X: boxerOneRightShoulderPose.X + 1, Y: boxerOneRightShoulderPose.Y}
+
+				// player 2
+				boxerTwoHead = "),: **"
+				boxerTwoLeftArm = "O══╯╯"
+				boxerTwoRightArm = "O══╮╮"
+				boxerTwoLeftShoulder = "()"
+				boxerTwoRightShoulder = "()"
+			case "two":
+
+			}
 		}
 	} else {
-		leftShoulder = "()"
-		rightShoulder = "()"
-		leftArm = "╭╭══O"
-		rightArm = "╰╰══O"
+		// player 1
+		boxerOneLeftArm = "╭╭══O"
+		boxerOneRightArm = "╰╰══O"
+		boxerOneLeftShoulder = "()"
+		boxerOneRightShoulder = "()"
+
+		// player 2
+		boxerTwoLeftArm = "O══╯╯"
+		boxerTwoRightArm = "O══╮╮"
+		boxerTwoLeftShoulder = "()"
+		boxerTwoRightShoulder = "()"
 	}
 
 	//spawn cage
 	spawnCage(cage)
 
-	// clear screen first
-	//fmt.Print("\033[2J")
-
+	// player 1
 	// Shoulders
-	fmt.Printf("\033[%d;%dH%s%s%s", leftShoulderPose.Y, leftShoulderPose.X, boxer.Color, leftShoulder, reset)
-	fmt.Printf("\033[%d;%dH%s%s%s", rightShoulderPose.Y, rightShoulderPose.X, boxer.Color, rightShoulder, reset)
+	if boxerOneLeftShoulder != "" {
+		fmt.Printf(
+			"\033[%d;%dH%s%s%s", boxerOneLeftShoulderPose.Y, boxerOneLeftShoulderPose.X, boxerOne.Color, boxerOneLeftShoulder, reset)
+	}
+	if boxerOneRightShoulder != "" {
+		fmt.Printf(
+			"\033[%d;%dH%s%s%s", boxerOneRightShoulderPose.Y, boxerOneRightShoulderPose.X, boxerOne.Color, boxerOneRightShoulder, reset)
+	}
 
 	// Arms
-	fmt.Printf("\033[%d;%dH%s%s%s", leftArmPose.Y, leftArmPose.X, boxer.Color, leftArm, reset)
-	fmt.Printf("\033[%d;%dH%s%s%s", rightArmPose.Y, rightArmPose.X, boxer.Color, rightArm, reset)
+	if boxerOneLeftArm != "" {
+		fmt.Printf("\033[%d;%dH%s%s%s", boxerOneLeftArmPose.Y, boxerOneLeftArmPose.X, boxerOne.Color, boxerOneLeftArm, reset)
+	}
+	if boxerOneRightArm != "" {
+		fmt.Printf("\033[%d;%dH%s%s%s", boxerOneRightArmPose.Y, boxerOneRightArmPose.X, boxerOne.Color, boxerOneRightArm, reset)
+	}
 
 	// Head
-	fmt.Printf("\033[%d;%dH%s%s%s", boxer.Pose.Y, boxer.Pose.X, boxer.Color, head, reset)
+	fmt.Printf("\033[%d;%dH%s%s%s", boxerOne.Pose.Y, boxerOne.Pose.X, boxerOne.Color, boxerOneHead, reset)
+
+	// player 2
+	// Shoulders
+	if boxerTwoLeftShoulder != "" {
+		fmt.Printf(
+			"\033[%d;%dH%s%s%s", boxerTwoLeftShoulderPose.Y, boxerTwoLeftShoulderPose.X, boxerTwo.Color, boxerTwoLeftShoulder, reset)
+	}
+	if boxerTwoRightShoulder != "" {
+		fmt.Printf(
+			"\033[%d;%dH%s%s%s", boxerTwoRightShoulderPose.Y, boxerTwoRightShoulderPose.X, boxerTwo.Color, boxerTwoRightShoulder, reset)
+	}
+
+	// Arms
+	if boxerTwoLeftArm != "" {
+		fmt.Printf("\033[%d;%dH%s%s%s", boxerTwoLeftArmPose.Y, boxerTwoLeftArmPose.X, boxerTwo.Color, boxerTwoLeftArm, reset)
+	}
+	if boxerTwoRightArm != "" {
+		fmt.Printf("\033[%d;%dH%s%s%s", boxerTwoRightArmPose.Y, boxerTwoRightArmPose.X, boxerTwo.Color, boxerTwoRightArm, reset)
+	}
+
+	// Head
+	fmt.Printf("\033[%d;%dH%s%s%s", boxerTwo.Pose.Y, boxerTwo.Pose.X, boxerTwo.Color, boxerTwoHead, reset)
 
 	// Move cursor below art
-	fmt.Printf("\033[%d;1H", rightShoulderPose.Y+10)
-}
-
-func upArmHit(boxer Boxer) {
-
+	fmt.Printf("\033[%d;1H", cage.Limit.Y+10)
 }
 
 func main() {
@@ -102,8 +229,8 @@ func main() {
 	cage := Cage{Limit: cageLimit, Color: "\033[44m"}
 
 	// Spawn boxer
-	headPose := Position{X: 10, Y: 10}
-	boxer := Boxer{Pose: headPose, Color: "\033[31m"}
+	boxerOne := Boxer{Pose: Position{X: 10, Y: 10}, Color: "\033[31m"}
+	boxerTwo := Boxer{Pose: Position{X: 30, Y: 10}, Color: "\033[32m"}
 	//spawnBoxer(cage, boxer, "")
 
 	// put terminal in raw mode (no buffering, instant keypresses)
@@ -123,7 +250,7 @@ func main() {
 		os.Exit(0)
 	}()
 
-	spawnBoxer(cage, boxer, "")
+	spawnBoxers(cage, boxerOne, boxerTwo, "")
 
 	buf := make([]byte, 3) // arrow keys send 3 bytes like ESC [ A
 	for {
@@ -134,29 +261,53 @@ func main() {
 
 		switch string(buf[:n]) {
 		case "\033[A": // up
-			if boxer.Pose.Y > 4 {
-				boxer.Pose.Y--
-				spawnBoxer(cage, boxer, "")
+			if boxerOne.Pose.Y > 4 {
+				boxerOne.Pose.Y--
+				spawnBoxers(cage, boxerOne, boxerTwo, "")
 			}
 		case "\033[B": // down
-			if boxer.Pose.Y < cageLimit.Y-4 {
-				boxer.Pose.Y++
-				spawnBoxer(cage, boxer, "")
+			if boxerOne.Pose.Y < cageLimit.Y-4 {
+				boxerOne.Pose.Y++
+				spawnBoxers(cage, boxerOne, boxerTwo, "")
 			}
 		case "\033[C": // right
-			if boxer.Pose.X < cageLimit.X-6 {
-				boxer.Pose.X++
-				spawnBoxer(cage, boxer, "")
+			if boxerOne.Pose.X < cageLimit.X-6 {
+				boxerOne.Pose.X++
+				spawnBoxers(cage, boxerOne, boxerTwo, "")
 			}
 		case "\033[D": // left
-			if boxer.Pose.X > 2 {
-				boxer.Pose.X--
-				spawnBoxer(cage, boxer, "")
+			if boxerOne.Pose.X > 2 {
+				boxerOne.Pose.X--
+				spawnBoxers(cage, boxerOne, boxerTwo, "")
 			}
-		//case "s":
-		//	upArmHit(boxer)
-		//case "x":
-		//	downArmHit(boxer)
+		case "s":
+			spawnBoxers(cage, boxerOne, boxerTwo, "upInit")
+			time.Sleep(100 * time.Millisecond)
+			spawnBoxers(cage, boxerOne, boxerTwo, "up")
+			time.Sleep(100 * time.Millisecond)
+			spawnBoxers(cage, boxerOne, boxerTwo, "upHit")
+			time.Sleep(100 * time.Millisecond)
+			spawnBoxers(cage, boxerOne, boxerTwo, "")
+		case "x":
+			spawnBoxers(cage, boxerOne, boxerTwo, "downInit")
+			time.Sleep(100 * time.Millisecond)
+			spawnBoxers(cage, boxerOne, boxerTwo, "down")
+			time.Sleep(100 * time.Millisecond)
+			spawnBoxers(cage, boxerOne, boxerTwo, "")
+		case "k":
+			spawnBoxers(cage, boxerOne, boxerTwo, "upInit")
+			time.Sleep(100 * time.Millisecond)
+			spawnBoxers(cage, boxerOne, boxerTwo, "up")
+			time.Sleep(100 * time.Millisecond)
+			spawnBoxers(cage, boxerOne, boxerTwo, "upHit")
+			time.Sleep(100 * time.Millisecond)
+			spawnBoxers(cage, boxerOne, boxerTwo, "")
+		case "m":
+			spawnBoxers(cage, boxerOne, boxerTwo, "downInit")
+			time.Sleep(100 * time.Millisecond)
+			spawnBoxers(cage, boxerOne, boxerTwo, "down")
+			time.Sleep(100 * time.Millisecond)
+			spawnBoxers(cage, boxerOne, boxerTwo, "")
 		case "q", "Q": // quit
 			clearScreen()
 			return
